c26ceb8c924180e3eecc3f357ad970dbd9c5ea27
GustavoGenari <gustavogenari92@gmail.com>
2025-10-22 15:26:07 -0300

    Adiciona o repositories

---
+++ src/main/java/com/carteira/repositories/InvestimentoRepository.java
@@ -0,0 +1,10 @@
+package com.carteira.repositories;
+
+import com.carteira.entities.Investimento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InvestimentoRepository extends JpaRepository<Investimento, Long> {
+
+}
